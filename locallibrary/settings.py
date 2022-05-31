9ed68faa6620c59bdbad5a57862f12ce504c84ad
mahmoud20212 <occee123@gmail.com>
2022-05-31 16:44:49 +0300

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
-    'locallibrary159.herokuapp.com','127.0.0.1'
+    'stark-garden-32627.herokuapp.com','127.0.0.1'
 ]
 
 
